47325b62387a11fd964e64c94fc9c56200afaa15
Milovane <122837001+Milovane@users.noreply.github.com>
2023-02-24 16:23:32 +0100

    Added movie model

---
+++ src/main/java/com/mcdb/MovieCharacters/models/Movie.java
@@ -0,0 +1,79 @@
+package com.mcdb.MovieCharacters.models;
+
+import java.util.Set;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private String genre;
+    private String year;
+    private String director;
+    private String poster; //detta ska vara ett URL till en bild på en poster, type är nog fel
+    private String trailer; //detta ska vara ett URL till en trailer på youtube, type kan vara fel
+    private Set<Character> characters;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public Set<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(Set<Character> characters) {
+        this.characters = characters;
+    }
+}
